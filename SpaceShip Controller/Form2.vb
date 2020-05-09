7680b99ae6ecef06760f49498af92d565ae29933
CTECH9 <43302509+CTECH9@users.noreply.github.com>
2020-05-09 19:22:45 -0400

    Initial commit

---
+++ SpaceShip Controller/Form2.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form2
+    Private Sub Button3_Click(sender As System.Object, e As System.EventArgs) Handles Button3.Click
+        Me.Hide()
+    End Sub
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        MsgBox("Firing Missile.", MsgBoxStyle.Information + MsgBoxStyle.OkOnly, "Missile information")
+    End Sub
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        MsgBox("Firing Phasers.", MsgBoxStyle.Information + MsgBoxStyle.OkOnly, "Phaser Information")
+    End Sub
+End Class
---
+++ SpaceShip Controller/My Project/AssemblyInfo.vb
@@ -0,0 +1,39 @@
+﻿Imports System.Resources
+
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("WindowsApplication1")> 
+<Assembly: AssemblyDescription("Control a spaceship.")> 
+<Assembly: AssemblyCompany("CTECH 9 Industries")> 
+<Assembly: AssemblyProduct("The SpaceShip Controller")> 
+<Assembly: AssemblyCopyright("Copyright ©  2017")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)> 
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("39a0c2b3-31c0-4c34-afa9-a94753a79e04")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.1.0.0")> 
+<Assembly: AssemblyFileVersion("1.1.0.0")> 
+
+<Assembly: NeutralResourcesLanguageAttribute("en-US")> 
